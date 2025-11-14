259d691b79bf5c79e97baf2214a93eaea2c5f163
jalenjab <jalenjab@Jalen.localdomain>
2025-11-14 17:47:49 -0500

    fixed lls

--- ABDQ.hpp
+++ ABDQ.hpp
@@ -3,7 +3,6 @@
 #include "Interfaces.hpp"
 #include <cstddef>
 #include <stdexcept>
-#include <utility>
 
 template <typename T>
 class ABDQ : public DequeInterface<T> {
@@ -18,9 +17,8 @@ private:
     void resize() {
         std::size_t new_capacity = capacity_ * SCALE_FACTOR;
         T* new_data = new T[new_capacity];
-        for (std::size_t i = 0; i < size_; ++i) {
+        for (std::size_t i = 0; i < size_; ++i)
             new_data[i] = data_[(front_ + i) % capacity_];
-        }
         delete[] data_;
         data_ = new_data;
         capacity_ = new_capacity;
@@ -30,22 +28,28 @@ private:
 
 public:
     ABDQ() : data_(new T[1]), capacity_(1), size_(0), front_(0), back_(0) {}
-    explicit ABDQ(std::size_t capacity) : data_(new T[capacity > 0 ? capacity : 1]),
-        capacity_(capacity > 0 ? capacity : 1), size_(0), front_(0), back_(0) {}
-    ABDQ(const ABDQ& other) : data_(new T[other.capacity_]), capacity_(other.capacity_),
-        size_(other.size_), front_(0), back_(other.size_) {
-        for (std::size_t i = 0; i < size_; ++i) {
+    explicit ABDQ(std::size_t capacity)
+        : data_(new T[capacity > 0 ? capacity : 1]),
+          capacity_(capacity > 0 ? capacity : 1),
+          size_(0), front_(0), back_(0) {}
+
+    ABDQ(const ABDQ& other)
+        : data_(new T[other.capacity_]), capacity_(other.capacity_),
+          size_(other.size_), front_(0), back_(other.size_) {
+        for (std::size_t i = 0; i < size_; ++i)
             data_[i] = other.data_[(other.front_ + i) % other.capacity_];
-        }
     }
-    ABDQ(ABDQ&& other) noexcept : data_(other.data_), capacity_(other.capacity_),
-        size_(other.size_), front_(other.front_), back_(other.back_) {
+
+    ABDQ(ABDQ&& other) noexcept
+        : data_(other.data_), capacity_(other.capacity_),
+          size_(other.size_), front_(other.front_), back_(other.back_) {
         other.data_ = nullptr;
-        other.size_ = 0;
         other.capacity_ = 0;
+        other.size_ = 0;
         other.front_ = 0;
         other.back_ = 0;
     }
+
     ABDQ& operator=(const ABDQ& other) {
         if (this != &other) {
             delete[] data_;
@@ -54,12 +58,12 @@ public:
             front_ = 0;
             back_ = size_;
             data_ = new T[capacity_];
-            for (std::size_t i = 0; i < size_; ++i) {
+            for (std::size_t i = 0; i < size_; ++i)
                 data_[i] = other.data_[(other.front_ + i) % other.capacity_];
-            }
         }
         return *this;
     }
+
     ABDQ& operator=(ABDQ&& other) noexcept {
         if (this != &other) {
             delete[] data_;
@@ -69,53 +73,55 @@ public:
             front_ = other.front_;
             back_ = other.back_;
             other.data_ = nullptr;
-            other.size_ = 0;
             other.capacity_ = 0;
+            other.size_ = 0;
             other.front_ = 0;
             other.back_ = 0;
         }
         return *this;
     }
-    ~ABDQ() override {
-        delete[] data_;
-    }
+
+    ~ABDQ() override { delete[] data_; }
 
     void pushFront(const T& item) override {
-        if (size_ == capacity_) resize();
+        if (size_ >= capacity_) resize();
         front_ = (front_ + capacity_ - 1) % capacity_;
         data_[front_] = item;
         ++size_;
     }
+
     void pushBack(const T& item) override {
-        if (size_ == capacity_) resize();
+        if (size_ >= capacity_) resize();
         data_[back_] = item;
         back_ = (back_ + 1) % capacity_;
         ++size_;
     }
+
     T popFront() override {
         if (size_ == 0) throw std::runtime_error("Deque is empty");
-        T item = data_[front_];
+        T val = data_[front_];
         front_ = (front_ + 1) % capacity_;
         --size_;
-        return item;
+        return val;
     }
+
     T popBack() override {
         if (size_ == 0) throw std::runtime_error("Deque is empty");
         back_ = (back_ + capacity_ - 1) % capacity_;
-        T item = data_[back_];
+        T val = data_[back_];
         --size_;
-        return item;
+        return val;
     }
+
     const T& front() const override {
         if (size_ == 0) throw std::runtime_error("Deque is empty");
         return data_[front_];
     }
+
     const T& back() const override {
         if (size_ == 0) throw std::runtime_error("Deque is empty");
-        std::size_t idx = (back_ + capacity_ - 1) % capacity_;
-        return data_[idx];
+        return data_[(back_ + capacity_ - 1) % capacity_];
     }
-    std::size_t getSize() const noexcept override {
-        return size_;
-    }
+
+    std::size_t getSize() const noexcept override { return size_; }
 };
--- LLS.hpp
+++ LLS.hpp
@@ -1,5 +1,4 @@
 #pragma once
-
 #include "Interfaces.hpp"
 #include "LinkedList.hpp"
 #include <stdexcept>
@@ -9,79 +8,30 @@ class LLS : public StackInterface<T> {
 private:
     LinkedList<T> list;
 
-public:#pragma once
+public:
+    LLS() = default;
 
-#include "Interfaces.hpp"
-#include "LinkedList.hpp"
-#include <stdexcept>
-
-    template <typename T>
-    class LLS : public StackInterface<T> {
-    private:
-        LinkedList<T> list;
-
-    public:
-        // Constructor
-        LLS() = default;
-
-        // Push item onto stack (top = head)
-        void push(const T& item) override {
-            list.addHead(item);
-        }
-
-        // Remove and return top item
-        T pop() override {
-            if (list.getCount() == 0)
-                throw std::runtime_error("Stack is empty");
-            T value = list.getHead()->data;
-            list.removeHead();
-            return value;
-        }
-
-        // Return top item without removing
-        T peek() const override {
-            if (list.getCount() == 0)
-                throw std::runtime_error("Stack is empty");
-            return list.getHead()->data;
-        }
-
-        // Return number of items
-        std::size_t getSize() const noexcept override {
-            return list.getCount();
-        }
-    };
-
-    // Constructor
-    LLS() : list() {}
-
-    // Push item onto the stack
     void push(const T& item) override {
-        // Insert at head for LIFO behavior
-        list.addHead(item);
+        list.PushFront(item);
     }
 
-    // Remove and return the top item
     T pop() override {
-        auto headNode = list.getHead();
-        if (!headNode)
-            throw std::out_of_range("Stack is empty");
-
-        T value = headNode->data;
-        list.removeHead();
-        return value;
+        if (list.GetSize() == 0) throw std::runtime_error("Stack is empty");
+        T val = list.PeekFront();
+        list.PopFront();
+        return val;
     }
 
-    // Return the top item without removing
     T peek() const override {
-        auto headNode = list.getHead();
-        if (!headNode)
-            throw std::out_of_range("Stack is empty");
-
-        return headNode->data;
+        if (list.GetSize() == 0) throw std::runtime_error("Stack is empty");
+        return list.PeekFront();
     }
 
-    // Get the number of items in the stack
     std::size_t getSize() const noexcept override {
-        return list.getCount();
+        return list.GetSize();
+    }
+
+    void clear() {
+        list.Clear();
     }
 };
