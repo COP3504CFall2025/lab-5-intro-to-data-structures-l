72df2a49f31d9ea71e1a9859d5d730c54df03bcf
jalenjab <jalenjab@Jalen.localdomain>
2025-11-14 18:54:59 -0500

    interface

--- Interfaces.hpp
+++ Interfaces.hpp
@@ -2,30 +2,27 @@
 #include <cstddef>
 #include <stdexcept>
 
-
 template <typename T>
 class StackInterface {
 public:
     virtual ~StackInterface() = default;
     virtual void push(const T& item) = 0;
-    virtual void pop() = 0;
+    virtual T pop() = 0;               // Return the popped element
     virtual T& peek() = 0;
     virtual std::size_t getSize() const = 0;
     virtual bool isEmpty() const = 0;
 };
-
 
 template <typename T>
 class QueueInterface {
 public:
     virtual ~QueueInterface() = default;
     virtual void enqueue(const T& item) = 0;
-    virtual void dequeue() = 0;
+    virtual T dequeue() = 0;           // Return the dequeued element
     virtual T& peek() = 0;
     virtual std::size_t getSize() const = 0;
     virtual bool isEmpty() const = 0;
 };
-
 
 template <typename T>
 class DequeInterface {
@@ -33,8 +30,8 @@ public:
     virtual ~DequeInterface() = default;
     virtual void pushFront(const T& item) = 0;
     virtual void pushBack(const T& item) = 0;
-    virtual void popFront() = 0;
-    virtual void popBack() = 0;
+    virtual T popFront() = 0;          // Return the popped element
+    virtual T popBack() = 0;           // Return the popped element
     virtual T& front() = 0;
     virtual T& back() = 0;
     virtual std::size_t getSize() const = 0;
