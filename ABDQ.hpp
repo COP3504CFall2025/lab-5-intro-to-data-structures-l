d1171a5e99b6f3a10d53a8f67a829287b5995556
jalenjab <jalenjab@Jalen.localdomain>
2025-11-14 19:02:33 -0500

    throws

--- ABDQ.hpp
+++ ABDQ.hpp
@@ -16,10 +16,8 @@ private:
         std::size_t newCap = capacity_ * 2;
         T* newData = new T[newCap];
 
-
-        for (std::size_t i = 0; i < size_; ++i) {
+        for (std::size_t i = 0; i < size_; ++i)
             newData[i] = data_[(front_ + i) % capacity_];
-        }
 
         delete[] data_;
         data_ = newData;
@@ -36,10 +34,9 @@ public:
           back_(0),
           size_(0) {}
 
-    ~ABDQ() {
-        delete[] data_;
-    }
+    ~ABDQ() { delete[] data_; }
 
+    // Copy constructor
     ABDQ(const ABDQ& other)
         : data_(new T[other.capacity_]),
           capacity_(other.capacity_),
@@ -50,6 +47,24 @@ public:
             data_[i] = other.data_[(other.front_ + i) % other.capacity_];
     }
 
+    // Copy assignment
+    ABDQ& operator=(const ABDQ& other) {
+        if (this == &other) return *this;
+
+        T* newData = new T[other.capacity_];
+        for (std::size_t i = 0; i < other.size_; ++i)
+            newData[i] = other.data_[(other.front_ + i) % other.capacity_];
+
+        delete[] data_;
+        data_ = newData;
+        capacity_ = other.capacity_;
+        size_ = other.size_;
+        front_ = 0;
+        back_ = size_;
+        return *this;
+    }
+
+    // Move constructor
     ABDQ(ABDQ&& other) noexcept
         : data_(other.data_),
           capacity_(other.capacity_),
@@ -63,27 +78,43 @@ public:
         other.back_ = 0;
     }
 
+    // Move assignment
+    ABDQ& operator=(ABDQ&& other) noexcept {
+        if (this == &other) return *this;
+
+        delete[] data_;
+
+        data_ = other.data_;
+        capacity_ = other.capacity_;
+        size_ = other.size_;
+        front_ = other.front_;
+        back_ = other.back_;
+
+        other.data_ = nullptr;
+        other.size_ = 0;
+        other.capacity_ = 0;
+        other.front_ = 0;
+        other.back_ = 0;
+
+        return *this;
+    }
 
     void pushFront(const T& item) override {
-        if (size_ == capacity_)
-            resize();
+        if (size_ == capacity_) resize();
         front_ = (front_ == 0) ? capacity_ - 1 : front_ - 1;
         data_[front_] = item;
         ++size_;
     }
 
     void pushBack(const T& item) override {
-        if (size_ == capacity_)
-            resize();
+        if (size_ == capacity_) resize();
         data_[back_] = item;
         back_ = (back_ + 1) % capacity_;
         ++size_;
     }
 
-
     T popFront() override {
-        if (isEmpty())
-            throw std::runtime_error("Deque empty");
+        if (isEmpty()) throw std::runtime_error("Deque empty");
         T value = data_[front_];
         front_ = (front_ + 1) % capacity_;
         --size_;
@@ -91,33 +122,24 @@ public:
     }
 
     T popBack() override {
-        if (isEmpty())
-            throw std::runtime_error("Deque empty");
+        if (isEmpty()) throw std::runtime_error("Deque empty");
         back_ = (back_ == 0) ? capacity_ - 1 : back_ - 1;
         T value = data_[back_];
         --size_;
         return value;
     }
 
-
     T& front() override {
-        if (isEmpty())
-            throw std::runtime_error("Deque empty");
+        if (isEmpty()) throw std::runtime_error("Deque empty");
         return data_[front_];
     }
 
     T& back() override {
-        if (isEmpty())
-            throw std::runtime_error("Deque empty");
+        if (isEmpty()) throw std::runtime_error("Deque empty");
         std::size_t lastIndex = (back_ == 0) ? capacity_ - 1 : back_ - 1;
         return data_[lastIndex];
     }
 
-    std::size_t getSize() const override {
-        return size_;
-    }
-
-    bool isEmpty() const override {
-        return size_ == 0;
-    }
+    std::size_t getSize() const override { return size_; }
+    bool isEmpty() const override { return size_ == 0; }
 };
